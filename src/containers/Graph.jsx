85d994dce97a1d35d98e9a568a4f2c58e9014a2d
Casey Choiniere <caseychoiniere@gmail.com>
2017-12-26 14:35:22 -0800

    Initial commit

---
+++ src/containers/Graph.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/App.styl';
+
+class Graph extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Graph</h2>
+            </div>
+        );
+    }
+}
+
+export default Graph;
